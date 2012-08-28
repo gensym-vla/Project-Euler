1906c76c7a0f1c7e327374df288149e6b58f9fb8
Vedat Levi Alev <alev@in.tum.de>
2012-08-28 12:26:53 +0300

    PE54 solved

---
+++ PE54.py
@@ -0,0 +1,28 @@
+from math import sqrt
+from utils.sieve import is_prime
+
+def diag_num():
+    """
+    returns the diagonal values at the (i+1)-th iteration,
+    as the last value is always a square, we don't include it in the output
+    """
+    val  = [1,1,1]
+    incr = [2,4,6]
+    while True:
+        for i in range(3):
+            val[i] += incr[i]
+            incr[i] += 8
+        yield val
+
+def solve(r=0.1):
+    diag_count  = 1 # 1 sits in the center of the spiral
+    prime_count = 0 # 1 is not a prime 
+    for i,v in enumerate(diag_num()):
+        diag_count  += 4
+        prime_count += sum(map(is_prime, v))
+        if prime_count*1./diag_count < r:
+            # see remark on diag_num
+            print "Answer to PE54 is: {0}".format(2*(i + 1) + 1)
+            return 2*(i+1) - 1
+
+solve()
--- utils/sieve.py
+++ utils/sieve.py
@@ -1,6 +1,21 @@
 from math import sqrt
 from itertools import count
 
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n in [2,3]:
+        return True
+    if n % 6 != 1 and n % 6 != 5:
+        return False
+    lim = int(sqrt(n)) + 1
+    for f in range(5, lim, 6):
+        if n % f == 0 or n % (f + 2) == 0:
+            return False
+    return True
+
+
 def eratosthenes(n):
     """
     sieve of eratosthenes
